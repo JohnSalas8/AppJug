934d8b99e4f0e07eaca5825ecd8b3d27444e15f4
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-10 15:29:21 -0500

    Actualizando Repo

---
+++ driver/connection.php
@@ -0,0 +1,25 @@
+<?php
+
+    function connect_to_mysql() {
+        $server = "localhost";
+        $user = "root";
+        $pswd = "j24-cb09-ygo94";
+        $bdd = "CURP";
+
+        // ESTE TIPO DE CONEXION ES SIN USAR PHPMYADMIN
+        $connection = mysqli_connect($server, $user, $pswd) or die ("No se ha podido conectar con MySQL.");
+        mysqli_set_charset("UTF8", $connection);
+        mysqli_select_db($connection, $bdd) or die ("No existe la basde de datos con el nombre: $bdd");
+
+        return $connection;
+    }
+
+    function close_connection($connection) {
+        mysqli_close($connection);
+    }
+
+    function exe_query($connection, $query) {
+        return mysqli_query($connection, $query);
+    }
+        
+?>
