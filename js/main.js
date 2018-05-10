416259117100ddee2f56b725516240821a9d5801
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-10 16:20:25 -0500

    Actualizando

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ function randomColor() {
         Math.round(Math.random() * (colors.length-1) )
     ];
 }
+
+
+function checkUser() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("mssg").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "../driver/check_user.php", true);
+    xhttp.send();
+}
--- pages/sign_in.php
+++ pages/sign_in.php
@@ -12,14 +12,37 @@
     <script src="../js/main.js"></script>
 </head>
 <body>
-    
-    <!--
-        FALTA LA NAVBAR
-    -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a id="title-jug" class="navbar-brand" href="#">
+                    APP JUG
+                </a>
+
+                <script>
+                    randomColor();
+                </script>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <button class="btn navbar-btn" id="btn-games">Juegos</button>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="pages/sign_in.php"><span class="glyphicon glyphicon-user"></span> Iniciar Sesión</a></li>
+                    <li><a href="pages/create_count.php"><span class="glyphicon glyphicon-log-in"></span> Registrarse</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <div class="container">
-        <br><br>
-        <form action="">
+        <br><br><br>
+        <form action="post">
             <table>
                 <tr>
                     <td colspan="2" id="titulo-tabla">
